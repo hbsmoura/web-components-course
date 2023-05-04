27b80f3a9c04f2b733cb967f07e1890b8650fdb8
hbsmoura <87094634+hbsmoura@users.noreply.github.com>
2023-05-04 11:35:18 -0300

    Adding style

---
+++ assets/js/Components/DynamicTitle.js
@@ -0,0 +1,25 @@
+class DynamicTitle extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({mode: "open"});        
+
+        // criar base do component
+        const componentRoot = document.createElement('h1');
+        componentRoot.textContent = "Minha notícia";
+
+        // estilizar do component
+        const style = document.createElement('style');
+        style.textContent = `
+            h1 {
+                color: red;
+            }
+        `;
+
+        // enviar para a shadow
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+
+customElements.define('dynamic-title', DynamicTitle);
